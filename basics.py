baafa634e5267b1ccc6eeb827faf5cc136da6257
pingmado <75196272+ping2109@users.noreply.github.com>
2021-09-18 12:14:10 +0700

    Update basics.py

--- basics.py
+++ basics.py
@@ -1,34 +1,31 @@
 import datetime
-from datetime import date 
-from datetime import timedelta
-from datetime import datetime
 
-def get_birthday_from_user():
+def get_user_birthday():
     year = int('2007')
     month = int('09')
     day = int('21')
-    print('It looks like you were born on {}/{}/{}.'.format(month, day, year))
-    birthday = datetime.date(year, month, day)
+    birthday = datetime.datetime(year, month, day)
     return birthday
 
+def compute_birthday_difference(original_date, now):
+    date1 = now
+    date2 = datetime.datetime(now.year, original_date.month, original_date.day)
+    difference = date1 - date2
+    days = int(difference.total_seconds()/60/60/24)
+    return days
 
-def compute_days_between_dates(original_date, target_date):
-    this_year = datetime.date(target_date.year,original_date.month,original_date.day)
-    dt = this_year - target_date
-    return dt.days
-
-
-def print_birthday_information(days):
-    if days != 0:
-        if days > 1:
-            ub = 'unbirtdays'
-        elif days == 1:
-            ub = 'unbirthday'
-        print('Your birthday is {} {} away.'.format(days, ub))
-    elif days < 0:
-        print('You have already had your birthday, you sneaky fucking chump.')
+def print_bday_info(days):
+    if days < 0:
+        print('Your birthday is in {} days.'.format(-days))
+    elif days > 0:
+        print('Your birthday was {} days ago. Hope it was great!'.format(days))
     else:
-        print('Happy birthday!')
+        print('Happy Birthday!')
 
-        
+def main():
+    bday = get_user_birthday()
+    now = datetime.datetime.now()
+    days = compute_birthday_difference(bday, now)
+    print_bday_info(days)
+
 main()
