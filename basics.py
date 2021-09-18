253d4926efa8fb83ad6a70b6f44ab112f9eb0025
pingmado <75196272+ping2109@users.noreply.github.com>
2021-09-18 12:33:02 +0700

    Update basics.py

--- basics.py
+++ basics.py
@@ -1,9 +1,16 @@
+import os
 import datetime
+import telegram
+from telegram import Sticker, PhotoSize, TelegramError, StickerSet, MaskPosition, Bot
+from telegram.error import BadRequest
+
+bot_token = os.getenv('TOKEN')
+channel = os.getenv('TG_ID')
 
 def get_user_birthday():
-    year = int('2007')
-    month = int('09')
-    day = int('22')
+    year = os.getenv('YEAR')
+    month = os.getenv('MONTH')
+    day = os.getenv('DATE')
     birthday = datetime.datetime(year, month, day)
     return birthday
 
@@ -27,5 +34,16 @@ def main():
     now = datetime.datetime.now()
     days = compute_birthday_difference(bday, now)
     print_bday_info(days)
+    
+def send_tg_message():
+    try:
+        telegram_notify = telegram.Bot(bot_token)
+        message = print_bday_info(days)
+
+        telegram_notify.send_message(chat_id=channel, text=message, disable_web_page_preview=True,
+                                parse_mode='Markdown')
+    except Exception as ex:
+        print(ex)
 
 main()
+send_tg_message()
