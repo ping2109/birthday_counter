92c64c9164a032bba7954568608c977a1052fb16
pingmado <75196272+ping2109@users.noreply.github.com>
2021-09-18 12:08:41 +0700

    update smth

--- basics.py
+++ basics.py
@@ -1,23 +1,42 @@
+import datetime
 from datetime import date 
 from datetime import timedelta
 from datetime import datetime
 
-BIRTH_DAY = 21
-BIRTH_MONTH = 9
-
-current_year = str(datetime.now().year)
-next_year = str(datetime.now().year + 1)
-
-
-current_birthday = BIRTH_DAY + "/" + BIRTH_MONTH + "/" + current_year 
-next_birthday = BIRTH_DAY + "/" + BIRTH_MONTH + "/" + next_year
-
-current_bd_date = datetime.strptime(current_birthday, '%d/%m/%Y')
-next_bd_date = datetime.strptime(next_birthday, '%d/%m/%Y')
-current_date = datetime.now()
+def get_birthday_from_user():
+    year = int(2007)
+    month = int(09)
+    day = int(21)
+    print('It looks like you were born on {}/{}/{}.'.format(month, day, year))
+    birthday = datetime.date(year, month, day)
+    return birthday
 
 
-if current_bd_date < current_date :
-	print(str(next_bd_date - current_date) + " until your birthday.")
-else:
-    print(str(current_bd_date - current_date) + " until your birthday.")
+def compute_days_between_dates(original_date, target_date):
+    this_year = datetime.date(target_date.year,original_date.month,original_date.day)
+    dt = this_year - target_date
+    return dt.days
+
+
+def print_birthday_information(days):
+    if days != 0:
+        if days > 1:
+            ub = 'unbirtdays'
+        elif days == 1:
+            ub = 'unbirthday'
+        print('Your birthday is {} {} away.'.format(days, ub))
+    elif days < 0:
+        print('You have already had your birthday, you sneaky fucking chump.')
+    else:
+        print('Happy birthday!')
+
+
+def main():
+    print_header()
+    bday = get_birthday_from_user()
+    today = datetime.date.today()
+    number_of_days = int(compute_days_between_dates(bday, today))
+    print_birthday_information(number_of_days)
+
+
+main()
